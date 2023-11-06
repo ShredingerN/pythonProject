d81c823353adc873428d13d750338ba3a578f628
jo4670 <nataxawolf@gmail.com>
2023-11-06 17:08:51 +0300

    HW1

--- Seminar1/hw1_task2.py
+++ Seminar1/hw1_task2.py
@@ -11,13 +11,14 @@ import string
 def check_text(cmd, words):
     result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
     temp = result.stdout
-    out = ''.join([' ' if p in string.punctuation else p for p in temp])
     if result.returncode == 0:
+        out = ''.join([' ' if p in string.punctuation else p for p in temp])
         if words in out:
             return True
         else:
             return False
     return f'wrong command: {cmd}'
+
 
 
 if __name__ == '__main__':
